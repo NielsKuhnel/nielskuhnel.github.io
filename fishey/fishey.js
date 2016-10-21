09f3757c24344abcdf0313a6530372277c889363
niels.kuhnel <nkj@sitecore.net>
2016-10-21 04:06:07 +0200

    lol, go

--- fishey/fishey.js
+++ fishey/fishey.js
@@ -20,19 +20,19 @@ function fisheyeElements(elements, nx, maxSize, containerSize, boundX, boundY) {
     function updateElements(offsetX, offsetY) {
         var stepX = 1/nx;    
         var stepY = 1/ny;                      
-        var startX = scaleX * fishX((0) * stepX);                
+        var startX = scaleX * fishX((offsetX) * stepX);                
         for(var x = 0; x <= nx; x++) {            
-            var endX = scaleX * fishX((x+1) * stepX);
-            endX = x == nx ? Math.ceil(endX) : Math.floor(endX);                       
-            xs[x] = startX + offsetY;
+            var endX = scaleX * fishX((offsetX + x + 1) * stepX);
+            endX = x == nx ? Math.ceil(endX) : Math.floor(endX);                                   
+            xs[x] = startX;
             startX = endX;
         }        
         
-        var startY = scaleY * fishY((0) * stepY);                       
+        var startY = scaleY * fishY((offsetY) * stepY);                       
         for(var y = 0; y <= ny; y++) {
-            var endY = scaleY * fishY((y+1) * stepY);
+            var endY = scaleY * fishY((offsetY + y + 1) * stepY);
             endY = x == nx ? Math.ceil(endY) : Math.floor(endY);
-            ys[y] = startY + offsetY;
+            ys[y] = startY;
             startY = endY;
         }                         
         
@@ -52,11 +52,19 @@ function fisheyeElements(elements, nx, maxSize, containerSize, boundX, boundY) {
                     el.style.display = "block";  
                     transform[1] = posx;
                     transform[3] = posy;                   
-                    el.style.transform = transform.join("");                                         
-                    if( w >= maxSize[0] - 1 && h >= maxSize[1] - 1 ) {
+                    el.style.transform = transform.join("");
+                    
+                    var img = el.childNodes[0];
+                    transform[1] = -(1 - (w / maxSize[0]))*maxSize[0]/2;                                        
+                    transform[3] = -(1 - (h / maxSize[1]))*maxSize[1]/2;
+                    img.style.transform = transform.join("");
+                    
+                    if( w >= maxSize[0] - 10 && h >= maxSize[1] - 10 ) {
                         el.style.zIndex = 1;
                         el.style.border = "2px solid #ff0000";
+                        el.style.margin = "-1px 0 0 -1px";
                     } else {
+                        el.style.margin = "0";
                         el.style.border = "";
                         el.style.zIndex = 0;
                     }
@@ -67,25 +75,24 @@ function fisheyeElements(elements, nx, maxSize, containerSize, boundX, boundY) {
         }        
     }
         
-    var updateFish = function(pos, maxSize, min, max, fish, n, scale)  {
+    var updateFish = function(pos, maxSize, min, max, fish, n, scale, log)  {
         var p = (pos - min)/(max - min);
         fish.focus(Math.max(0, Math.min(1, p)));
+                
+        var overflow = p < 0 ? -p : p > 1 ? p - 1 : 0;        
         
-        var overflow = p < 0 ? -p : p > 1 ? p - 1 : 0;
-        overflow = 0*overflow*(max - min);
         //Gets the "d" for the fisheye that gives the desired max width        
-        var size = maxSize + overflow;
+        var size = maxSize;// + overflow;
         var d = (n*size - scale) / (scale - size);                            
         fish.setD(d, n, scale);
                         
-        return (p < 0 ? 1 : -1) * overflow;
+        return (p < 0 ? 1 : -1) * 4 * overflow;
     }
        
     var updateFunction = function(x, y) {
                 
-        var overflowX = updateFish(x, maxSize[0], boundX[0], boundX[1], fishX, nx, scaleX);
-        var overflowY = updateFish(y, maxSize[1], boundY[0], boundY[1], fishY, ny, scaleY);
-        
+        var overflowX = updateFish(x, maxSize[0], boundX[0], boundX[1], fishX, nx, scaleX, true);
+        var overflowY = updateFish(y, maxSize[1], boundY[0], boundY[1], fishY, ny, scaleY);        
         updateElements(overflowX, overflowY);
     };
     
